633ae2799607190305642e64337f8ebf9fa123c0
harshalkaigaonkar <harshalkaigaonkar31@gmail.com>
2022-02-08 12:18:36 +0530

    Nothing much

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,12 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import styles from './index.module.scss';
 
 export default function Home() {
   return (
-    <h1>Hello</h1>
+    <div>
+      <h1 className='red'>Hello</h1>
+    <h1 className={styles.red}><span>nested from scss</span> Hello again</h1>
+    </div>
   )
 }
